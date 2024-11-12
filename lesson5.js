0cdcab5ea31cc89b9b2a7d9a0bc568b809de7d3e
יונגרייז שירה <163@ladaat.local>
2024-11-12 16:55:29 +0200

    second file

--- lesson5.js
+++ lesson5.js
@@ -1,5 +1,15 @@
 
-for (let i = 0; i < catalog.length; i++)
-{
-    catalog[i]["id"]=i+100
+for (let i = 0; i < catalog.length; i++) {
+    catalog[i]["id"] = i + 100
+}
+
+function f1() {
+    let str = ""
+    let newname = document.querySelector("#n1").value
+    const arr1 = catalog.filter(item => item.title.includes(newname))
+    arr1.forEach(item => {
+        str += item.company + " " + item.artist + " " + item.country + " " + item.id + " " + item.price + " " + item.title + " " + item.year+"</br>"
+        
+    });
+    document.getElementById("n2").innerHTML=str
 }
