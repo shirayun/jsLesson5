56c338cf69bef30065cb822dd6799da857275370
יונגרייז שירה <163@ladaat.local>
2024-11-12 17:46:46 +0200

    third file

--- lesson5.js
+++ lesson5.js
@@ -8,8 +8,33 @@ function f1() {
     let newname = document.querySelector("#n1").value
     const arr1 = catalog.filter(item => item.title.includes(newname))
     arr1.forEach(item => {
-        str += item.company + " " + item.artist + " " + item.country + " " + item.id + " " + item.price + " " + item.title + " " + item.year+"</br>"
-        
+        str += item.company + " " + item.artist + " " + item.country + " " + item.id + " " + item.price + " " + item.title + " " + item.year + "</br>"
+
     });
-    document.getElementById("n2").innerHTML=str
+    document.getElementById("n2").innerHTML = str
+}
+const countries = catalog.map(item => item.country)
+const arr2countries = countries.filter((item, index) => !countries.includes(item, index + 1)).sort()
+for (const item of arr2countries) {
+    document.getElementById("countries").innerHTML += "<option>" + item + "</option>"
+}
+function f2()
+{
+    let str = ""
+    let newname = event.target.value
+    const arr1 = catalog.filter(item => item.country===newname)
+    arr1.forEach(item => {
+        str += item.company + " " + item.artist + " " + item.country + " " + item.id + " " + item.price + " " + item.title + " " + item.year + "</br>"
+
+    });
+    document.getElementById("n3").innerHTML = str
+}
+
+function f3()
+{
+    let first=document.getElementById("in1").value
+    let long=document.getElementById("in2").value
+    const disk=catalog.find(item=>item.year>first&&item.year<long)
+    let str=disk.artist+" "+disk.company+" "+disk.country+" "+disk.price+" "+disk.title+" "+disk.year+" "+disk.id
+    document.getElementById("n4").innerHTML = str
 }
